7c1aa6633463691128227a4728358c0314361a5f
Guan Yi Lian <glia0011@student.monash.edu>
2025-03-15 16:13:23 +1100

    - Made changes to base.html - Made changes to views.py - Made changes to auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,redirect, url_for, request, flash
+# from . import db
+# from flask_login import login_user, logout_user, login_required, current_user
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from .member import Member
 
 auth = Blueprint("auth",__name__)
 
-@auth.route("/login")
+@auth.route("/")
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("/login.html")
 
-@auth.route("/sign-up")
-def sign_up():
-    return "Sign Up"
+@auth.route("/signup_page", methods=["GET", "POST"])
+def signup():
+    return render_template("/signup.html")
 
 @auth.route("/logout")
 def log_out():
-    return "Log Out"
+    return redirect(url_for("/login.html"))
--- website/views.py
+++ website/views.py
@@ -2,33 +2,6 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 views = Blueprint("views",__name__)
 
-@views.route("/")
-@views.route("/home")
-def home():
-    return render_template("/home.html")
-
-@views.route("/signup_page", methods=["GET", "POST"])
-def signup():
-    if request.method == "POST":
-        first_name = request.form.get("first_name")
-        last_name = request.form.get("last_name")
-        birthday = request.form.get("birthday")
-        gender = request.form.get("gender")
-        email = request.form.get("email")
-        password = request.form.get("password")
-        confirm_password = request.form.get("confirm_password")
-
-        # Basic validation
-        if password != confirm_password:
-            flash("Passwords do not match!", "error")
-            return redirect(url_for("views.signup"))
-
-        # Here, you can store the data in a database
-        flash("Signup successful! You can now log in.", "success")
-        return redirect(url_for("views.home"))
-
-    return render_template("/signup.html")
-
 @views.route("/todo_list")
 def todo_list():
     return render_template("/todo_list.html")
