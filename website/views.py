02192a0490820113445c914bd907032720c684fa
Ming Ze Tang <mtan0132@student.monash.edu>
2025-03-15 15:10:11 +1100

    Modify signup html combine with flask and add the html in views python file

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 views = Blueprint("views",__name__)
 
@@ -7,7 +7,24 @@ def home():
     return "Home"
 
 @views.route("/")
-@views.route("/signup_page")
+@views.route("/signup_page", methods=["GET", "POST"])
 def signup():
-    return render_template("/signup.html")
+    if request.method == "POST":
+        first_name = request.form.get("first_name")
+        last_name = request.form.get("last_name")
+        birthday = request.form.get("birthday")
+        gender = request.form.get("gender")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm_password")
 
+        # Basic validation
+        if password != confirm_password:
+            flash("Passwords do not match!", "error")
+            return redirect(url_for("views.signup"))
+
+        # Here, you can store the data in a database
+        flash("Signup successful! You can now log in.", "success")
+        return redirect(url_for("views.home"))
+
+    return render_template("/signup.html")
